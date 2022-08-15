7b06a1791e06e0091bfbbaf2c59f494459d7d64a
7rahib <rahibrashid002@gmail.com>
2022-08-15 15:57:35 +0600

    formatted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Donate from './Pages/Donate/Donate';
-import FundRaiser from './Pages/FundRaiser/FundRaiser';
-import Home from './Pages/Home/Home';
-import Footer from './Pages/Shared/Footer';
-import Navbar from './Pages/Shared/Navbar';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Donate from "./Pages/Donate/Donate";
+import FundRaiser from "./Pages/FundRaiser/FundRaiser";
+import Home from "./Pages/Home/Home";
+import Footer from "./Pages/Shared/Footer";
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/donate' element={<Donate />}></Route>
-        <Route path='/fundraiser' element={<FundRaiser />}></Route>
-      </Routes >
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/donate" element={<Donate />}></Route>
+        <Route path="/fundraiser" element={<FundRaiser></FundRaiser>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
